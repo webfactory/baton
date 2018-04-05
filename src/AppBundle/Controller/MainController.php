f09c5c73d83d693cc104c93f2b3df9bcf7975436
Konstantin Tieber <kt@webfactory.de>
2018-04-05 15:19:51 +0200

    Display new usage search form on main page

--- src/AppBundle/Controller/MainController.php
+++ src/AppBundle/Controller/MainController.php
@@ -2,29 +2,66 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Package;
+use AppBundle\Form\Type\SearchPackageType;
 use Doctrine\Common\Persistence\ObjectRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\FormFactory;
 
-class MainController extends Controller
+/**
+ * @Route(service="app.controller.main")
+ */
+class MainController
 {
     /**
      * @var ObjectRepository
      */
     private $projectRepo;
 
-    public function __construct(ObjectRepository $projectRepository)
+    /**
+     * @var FormFactory
+     */
+    private $formFactory;
+
+    public function __construct(ObjectRepository $projectRepository, FormFactory $formFactory)
     {
         $this->projectRepo = $projectRepository;
+        $this->formFactory = $formFactory;
     }
 
     /**
      * @Route("/", name="main")
-     *"@Template()
+     * @Template()
      */
     public function mainAction()
     {
-        return ['projects' => $this->projectRepo->findAll()];
+        $searchPackageForm = $this->formFactory->create(SearchPackageType::class, null, ['method' => 'POST']);
+
+        return [
+            'projects' => $this->projectRepo->findAll(),
+            'searchPackageForm' => $searchPackageForm->createView(),
+        ];
+    }
+
+    /**
+     * @Route("/package/{packageSlug}.{id}/{operator}/{versionString}", name="searchProjectsThatUsePackage")
+     * @ParamConverter("package", class="AppBundle:Package")
+     * @Template()
+     */
+    public function searchProjectsThatUsePackageAction(Package $package, $operator, $versionString)
+    {
+        $projects = [];
+        foreach($package->getVersionsThatMatchVersionConstraint($operator, $versionString) as $packageVersion) {
+            foreach($packageVersion->getProjects() as $project) {
+                $projects[] = $project;
+            }
+        }
+        return [
+            'package' => $package,
+            'versionConstraint' => $operator . ' ' . $versionString,
+            'projects' => $projects,
+        ];
     }
 }
