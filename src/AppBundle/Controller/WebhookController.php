5badb29f29bb479b3c164a3b767940c485f68a1b
Konstantin Tieber <kt@webfactory.de>
2018-04-05 15:26:38 +0200

    Add webhook route for synchronizing vcsrepositories

---
+++ src/AppBundle/Controller/WebhookController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Task\ImportProjectTask;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+/**
+ * @Route(service="app.controller.webhook")
+ */
+class WebhookController
+{
+    /**
+     * @var ImportProjectTask
+     */
+    private $importProjectTask;
+
+    public function __construct(ImportProjectTask $importProjectTask)
+    {
+        $this->importProjectTask = $importProjectTask;
+    }
+
+    /**
+     * @Route("/webhook", name="webhook")
+     * @Method({"POST"})
+     */
+    public function updateAction(Request $request) {
+        set_time_limit(500);
+
+        // This works for the Kiln webhook API as well as for GitHub webhooks of content-type "application/x-www-form-urlencoded"
+        if ($payload = $request->get('payload')) {
+            $payload = json_decode($payload);
+            if (isset($payload->repository) && $payload->repository && $payload->repository->url) {
+                $this->importProjectTask->run($payload->repository->url);
+            }
+        }
+
+        return new Response();
+    }
+}
