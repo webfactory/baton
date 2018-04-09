dcd2e08ee7b5ea3faf75904773e188c9002efa64
Konstantin Tieber <kt@webfactory.de>
2018-04-09 16:58:27 +0200

    Create PackageVersionFetcher to extract PackageVersion objects from composer.lock file

---
+++ src/AppBundle/ProjectImport/PackageVersionFetcher.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace AppBundle\ProjectImport;
+
+
+use AppBundle\Entity\PackageVersion;
+use Doctrine\Common\Collections\ArrayCollection;
+
+class PackageVersionFetcher
+{
+    /** @var PackageProviderInterface */
+    private $packageProvider;
+
+    /** @var ComposerPackageFetcher */
+    private $composerPackageFetcher;
+
+    public function __construct(PackageProviderInterface $packageProvider,ComposerPackageFetcher $composerPackageFetcher)
+    {
+        $this->packageProvider = $packageProvider;
+        $this->composerPackageFetcher = $composerPackageFetcher;
+    }
+
+    /**
+     * @param string $vcsUrl
+     * @return ArrayCollection|PackageVersion[]
+     */
+    public function fetch($vcsUrl)
+    {
+        $usages = new ArrayCollection();
+        foreach($this->composerPackageFetcher->fetchPackages($vcsUrl) as $composerPackage) {
+            $package = $this->packageProvider->providePackage($composerPackage->getName());
+            $usages->add(
+                $package->getVersion($composerPackage->getPrettyVersion())
+            );
+        }
+
+        return $usages;
+    }
+}
