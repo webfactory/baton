0a630e530b4f46dd6df21379136a69f5b7e0123c
Konstantin Tieber <kt@webfactory.de>
2018-04-10 10:31:20 +0200

    Fix doctrine entity fixtures

--- src/AppBundle/DataFixtures/AppFixtures.php
+++ src/AppBundle/DataFixtures/AppFixtures.php
@@ -3,7 +3,6 @@
 namespace AppBundle\DataFixtures;
 
 use AppBundle\Entity\Package;
-use AppBundle\Entity\PackageVersion;
 use AppBundle\Entity\Project;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Collections\ArrayCollection;
@@ -13,19 +12,24 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        for ($i = 0; $i < 3; $i++) {
+        $versionStrings = ["1.0.0", "2.0.0", "3.0.0"];
+        for ($i = 0; $i < 2; $i++) {
             $project = new Project(
                 uniqid('', false)
             );
+            $project->setVcsUrl('https://foo.git');
 
-            $project->setUsedPackageVersions(
-                new ArrayCollection(
-                    new PackageVersion(
-                        '1.0.0',
-                        new Package(uniqid('webfactory/', false))
-                    )
-                )
-            );
+            $packages = [];
+            for ($ii = 0; $ii < 5; $ii++) {
+                $packages[] = new Package(uniqid('webfactory/', false));
+            }
+
+
+            $packageVersions = new ArrayCollection();
+            foreach($packages as $package) {
+                $packageVersions->add($package->getVersion(array_rand(array_flip($versionStrings), 1)));
+            }
+            $project->setUsedPackageVersions($packageVersions);
 
             $manager->persist($project);
         }
