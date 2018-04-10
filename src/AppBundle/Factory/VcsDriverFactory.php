2f494ebd698f0ab31fed50be431865baf983c663
Konstantin Tieber <kt@webfactory.de>
2018-04-10 09:09:53 +0200

    Throw exception in VcsDriverFactory when repository communication fails
    I decoupled the creation of the driver object from the
    Composer\VcsRepository class since it added unnecessary bulk
    and the VcsDriverInterface defines a supports($url) method.

--- src/AppBundle/Factory/VcsDriverFactory.php
+++ src/AppBundle/Factory/VcsDriverFactory.php
@@ -2,10 +2,10 @@
 
 namespace AppBundle\Factory;
 
+use AppBundle\Exception\InsufficientVcsAccessException;
 use Composer\Factory;
 use Composer\IO\NullIO;
 use Composer\Repository\Vcs\VcsDriverInterface;
-use Composer\Repository\VcsRepository;
 
 class VcsDriverFactory
 {
@@ -15,40 +15,63 @@ class VcsDriverFactory
     /** @var string */
     private $kilnOAuthToken;
 
-    public function __construct($githubOAuthToken, $kilnOAuthToken)
+    /** @var array ['platform' => VcsDriverInterface class ] */
+    private $drivers;
+
+    public function __construct($githubOAuthToken, $kilnOAuthToken, array $drivers = null)
     {
         $this->githubOAuthToken = $githubOAuthToken;
         $this->kilnOAuthToken = $kilnOAuthToken;
+        $this->drivers = $drivers ?: [
+            'github' => 'Composer\Repository\Vcs\GitHubDriver',
+            'kiln' => 'AppBundle\Driver\KilnDriver',
+            'gitlab' => 'Composer\Repository\Vcs\GitLabDriver',
+            'git-bitbucket' => 'Composer\Repository\Vcs\GitBitbucketDriver',
+            'hg-bitbucket' => 'Composer\Repository\Vcs\HgBitbucketDriver',
+            'git' => 'Composer\Repository\Vcs\GitDriver',
+            'hg' => 'Composer\Repository\Vcs\HgDriver'
+        ];
     }
 
     /**
      * @param string $vcsUrl
      * @return VcsDriverInterface
+     * @throws InsufficientVcsAccessException
      */
-    public function getDriver($vcsUrl){
-        //TODO: don't depend on/support platforms such as GitHub or Kiln but use VCS level Drivers like GitDriver/HgDriver
+    public function getDriver($vcsUrl)
+    {
+        $composerConfig = Factory::createConfig();
+        $io = $this->getIO();
+
+        /** @var VcsDriverInterface $driver */
+        foreach ($this->drivers as $driver) {
+            if ($driver::supports($io, $composerConfig, $vcsUrl)) {
+                try {
+                    $driver = new $driver(['url' => $vcsUrl], $io, $composerConfig);
+                    $driver->initialize();
+                } catch (\RuntimeException $exception) {
+                    throw new InsufficientVcsAccessException(
+                        'Failed to communicate with repository. Check that you have sufficient access with your authentication method.', 0, $exception
+                    );
+                }
+
+                return $driver;
+            }
+        }
+    }
+
+    /**
+     * @return NullIO
+     */
+    private function getIO()
+    {
         $io = new NullIO();
         if($this->githubOAuthToken !== null) {
             $io->setAuthentication('github.com', $this->githubOAuthToken, 'x-oauth-basic');
         }
-
-        /** @var VcsRepository $vcsRepository */
-        $vcsRepository = new VcsRepository(
-            ['url' => $vcsUrl, 'kiln-token' => $this->kilnOAuthToken],
-            $io,
-            Factory::createConfig(),
-            null,
-            [
-                'github' => 'Composer\Repository\Vcs\GitHubDriver',
-                'kiln' => 'AppBundle\Driver\KilnDriver',
-                'gitlab' => 'Composer\Repository\Vcs\GitLabDriver',
-                'git-bitbucket' => 'Composer\Repository\Vcs\GitBitbucketDriver',
-                'hg-bitbucket' => 'Composer\Repository\Vcs\HgBitbucketDriver',
-                'git' => 'Composer\Repository\Vcs\GitDriver',
-                'hg' => 'Composer\Repository\Vcs\HgDriver'
-            ]
-        );
-
-        return $vcsRepository->getDriver();
+        if($this->kilnOAuthToken !== null) {
+            $io->setAuthentication('webfactory.kilnhg.com', $this->kilnOAuthToken, 'x-oauth-basic');
+        }
+        return $io;
     }
 }
