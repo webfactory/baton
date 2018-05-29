9168443b20b4c49ff09f646d054cc4bb148a764c
Konstantin Tieber <kt@webfactory.de>
2018-05-29 19:54:37 +0200

    Define DatalistType theme in separately from main view

--- src/AppBundle/Form/Type/DatalistType.php
+++ src/AppBundle/Form/Type/DatalistType.php
@@ -5,6 +5,10 @@ namespace AppBundle\Form\Type;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 
+/**
+ * Wrapper for the EntityType to use it with HTML datalist tag
+ * see AppBundle/Resources/views/form/fields.html.twig
+ */
 class DatalistType extends AbstractType
 {
     public function getParent() {
