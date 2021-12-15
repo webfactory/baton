525358ca938e9f808976a28d62f90690e7e40778
relthyg <mg@webfactory.de>
2021-12-15 13:49:42 +0100

    Add entrypoint for php image

---
+++ docker/php/docker-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+	set -- php-fpm "$@"
+fi
+
+if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
+  composer install
+  bin/console doctrine:database:create --if-not-exists
+  bin/console doctrine:schema:update --force
+  yarn install
+  gulp compile
+fi
+
+exec docker-php-entrypoint "$@"
