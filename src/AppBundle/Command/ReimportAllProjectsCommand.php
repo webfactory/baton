5bf3f91e87b8538806cdd5004b83a465f4ff16d2
janopae <8160319+janopae@users.noreply.github.com>
2025-07-04 10:54:27 +0000

    Fix CS with PHP-CS-Fixer

--- src/AppBundle/Command/ReimportAllProjectsCommand.php
+++ src/AppBundle/Command/ReimportAllProjectsCommand.php
@@ -5,18 +5,15 @@ namespace AppBundle\Command;
 use AppBundle\Entity\Repository\ProjectRepository;
 use AppBundle\ProjectImport\ImportProjectTask;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class ReimportAllProjectsCommand extends Command
 {
-
     public function __construct(
         private readonly ImportProjectTask $importProjectTask,
         private readonly ProjectRepository $projectRepository,
-    )
-    {
+    ) {
         parent::__construct();
     }
 
