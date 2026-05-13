913e274a48d2b6987246a4407d5f3d441cd1db0c
Malte Wunsch <mw@webfactory.de>
2026-05-13 23:42:43 +0200

    declare(strict_types=1) in all PHP files

--- src/App/Form/Type/DatalistType.php
+++ src/App/Form/Type/DatalistType.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Form\Type;
 
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
--- src/routing.php
+++ src/routing.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 return function (RoutingConfigurator $routes): void {
