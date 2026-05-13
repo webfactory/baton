2664e31790821b39dd99c2d2f4abbbf9ce55bd63
Malte Wunsch <mw@webfactory.de>
2026-05-13 17:43:59 +0200

    Remove activation of deprecated WebServerBundle

--- config/bundles.php
+++ config/bundles.php
@@ -7,7 +7,6 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['development' => true, 'test' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
-    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['all' => true],
     AppBundle\AppBundle::class => ['all' => true],
 
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['development' => true, 'test' => true],
